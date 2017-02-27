3d39b585c549b65c62f37d51bf2a493b2b546769
ccho0415 <ccho0415@gmail.com>
2017-02-27 11:12:17 -0500

    adding in matching function

--- app/data/friend.js
+++ app/data/friend.js
@@ -3,6 +3,11 @@ var friendArray = [
 	name: "Amumu",
 	photo: "https://images3.alphacoders.com/658/thumb-1920-658032.jpg",
 	scores : [5,5,5,5,5,5,5,5,5,5]
+},
+{
+	name: "Annie",
+	photo: "http://ddragon.leagueoflegends.com/cdn/img/champion/splash/Annie_10.jpg",
+	scores: [5,1,5,1,3,3,5,3,5,5]
 }
 ];
 module.exports = friendArray;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -5,12 +5,18 @@ module.exports = function(app){
 	});
 	app.post("/api/friends", function(req,res){
 		console.log(req.body);
-		friendData.push({
-			name: "'"+req.body.name+"'",
-			photo: "'"+req.body.photo+"'",
-			scores: req.body.scores
-		});
+		// friendData.push({
+		// 	name: req.body.name,
+		// 	photo: req.body.photo,
+		// 	scores: req.body.scores
+		// });
+		for (var i =1; i<friendData.length; i++){
+			var user1 = friendData[i].scores;
+			var user2 = req.body.scores;
+			user1.forEach(element)
+		}
+
 		res.json(true)
-		console.log(friendData);
+
 	});
 };
