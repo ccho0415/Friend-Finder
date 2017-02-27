163268b8d16d6d35226498d25d6d1d4fa54a3e0a
ccho0415 <ccho0415@gmail.com>
2017-02-27 10:47:08 -0500

    got it to properly post scores

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,12 +4,13 @@ module.exports = function(app){
 		res.json(friendData);
 	});
 	app.post("/api/friends", function(req,res){
-		friendArray.push({
+		console.log(req.body);
+		friendData.push({
 			name: "'"+req.body.name+"'",
 			photo: "'"+req.body.photo+"'",
-			scores: [req.body.q1,req.body.q2,req.body.q3,req.body.q4,req.body.q5,req.body.q6,req.body.q7,req.body.q8,req.body.q9,req.body.q10]
+			scores: req.body.scores
 		});
-		res.json(true);
-		console.log(req.body);
+		res.json(true)
+		console.log(friendData);
 	});
 };
